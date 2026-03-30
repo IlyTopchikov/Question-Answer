e6ff792a7559746bab4e9b570aff3e16031acec1
IlyTopchikov <ismoilovilyosbek2000@gmail.com>
2026-03-30 15:57:36 +0500

    Add SecurityContextHolder import to QaController

--- src/main/java/com/example/demo/web/QaController.java
+++ src/main/java/com/example/demo/web/QaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
