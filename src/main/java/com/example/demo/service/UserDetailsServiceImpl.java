b81c1d5d22554b505c164cd068abd727adcf87e6
IlyTopchikov <ismoilovilyosbek2000@gmail.com>
2026-03-30 15:25:02 +0500

    Create UserDetailsServiceImpl.java

---
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.AppUser;
+import com.example.demo.repository.AppUserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final AppUserRepository repo;
+
+    public UserDetailsServiceImpl(AppUserRepository repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        AppUser user = repo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден: " + username));
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
